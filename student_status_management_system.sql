cb5ad8d20e9b720459ba35ab86b817c200cc82c7
cxy <1295423769@qq.com>
2023-01-12 19:27:25 +0800

    commit student_status_management_system.sql

---
+++ student_status_management_system.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Professional v12.09 (64 bit)
+MySQL - 8.0.31 : Database - student_status_management_system
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`student_status_management_system` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `student_status_management_system`;
+
+/*Table structure for table `class` */
+
+DROP TABLE IF EXISTS `class`;
+
+CREATE TABLE `class` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `monitor` varchar(30) DEFAULT NULL,
+  `department_id` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `class` */
+
+insert  into `class`(`id`,`name`,`monitor`,`department_id`) values ('06','计算机类06班','小何','10220');
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `department` */
+
+insert  into `department`(`id`,`name`) values ('10220','计算机与大数据学院');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int NOT NULL,
+  `name` varchar(10) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `academy` varchar(10) DEFAULT NULL,
+  `class` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`sex`,`birthday`,`academy`,`class`) values (102201625,'陈翔宇','男','2004-03-01','10220','06');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
